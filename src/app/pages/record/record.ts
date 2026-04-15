92e3df26e86f028bf4b40c2da6b282c9962f2de5
Leo <diegolyt29@gmail.com>
2026-04-15 16:53:36 -0500

    Integración de componente, paginas, componentes y dependencias de tailwind con interfaz básica de Index

---
+++ src/app/pages/record/record.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record',
+  imports: [],
+  templateUrl: './record.html',
+  styleUrl: './record.css',
+})
+export class Record {
+
+}
